fc1dd82e88e30b1e00cef9d53731439f2b7a2437
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-08 22:07:29 -0700

    Exercise

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int main(void)
-{yint s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }
