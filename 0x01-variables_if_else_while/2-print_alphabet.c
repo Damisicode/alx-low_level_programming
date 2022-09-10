ced0f75ff8dd377239a6e2ea1d211f82a37f4ed0
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-09 20:27:38 -0700

    Exercise

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {       char c = 'a';
-        while (c <= 'z')
-	{putchar(c);
-	c++;
-	}
-	putchar('\n');
-	return (0);
+while (c <= 'z')
+{putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
