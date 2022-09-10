93263c2ce23fa5f408ffabe372fa155517471390
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-09 21:28:04 -0700

    Exercise

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,12 @@
 int main(void)
 {       int i = 0;
 	while (i < 10)
-	{
-		putchar(i + '0');
-		i++;
-		putchar(',');
+	{putchar(i + '0');
+		if (i < 9)
+		{putchar(',');
 		putchar(' ');}
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
