8485c585b711a2cfedf9c90d03a91c02b4bfc7eb
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-09 21:02:52 -0700

    Exercise

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Description: function that prints hexadecimal number from 0 to f
+ * Return: 0
+ */
+
+int main(void)
+{	int i = 0;
+while (i < 10)
+{putchar('i' + '0');
+i++;
+}
+char c = 'a';
+while (c < 'g')
+putchar (c);
+putchar('\n');
+return (0);
+}
