0bb201bba520aacfcec370e39d4bae117a091a3a
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-09 18:40:48 -0700

    Exercise

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Main - main function that prints number from 0 to 9
+ * 
+ * Return 0
+ */
+
+int main(){
+
+	int i;
+	i=0;
+	while (i < 10) {
+		putchar(i);
+	        i++;
+	}
+	return (0);
+}
