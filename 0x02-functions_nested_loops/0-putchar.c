780d8a142cbe03f4f502d466b07a52cd67c5c0f9
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-14 12:01:47 -0700

    adding putcharr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,13 @@
  * Result: 0
  */
 
+int _putchar(char c)
+{
+printf("%s", c);
+}
+
 int main(void)
 {
-char put_char[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
-for (int i = 0; put_char[i]; i++)
-putchar(put_char[i]);
-putchar('\n');
+_putchar("_putchar");
 return (0);
 }
