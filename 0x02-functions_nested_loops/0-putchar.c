09e5ed5022154fcf6cd7bfdf83975bb2d7a53db7
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-14 12:44:15 -0700

    add to putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,12 @@
 
 int main(void)
 {
-  _putchar("_putchar");
+  char str[9] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+  int i=0;
+  while (str[i] != '\0')
+{  _putchar(str[i]);
+  i++;
+ }
+  _putchar('\n');
   return (0);
 }
