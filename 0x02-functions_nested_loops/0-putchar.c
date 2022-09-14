756586e288f47262f98b466490bad2cfa6514c76
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-14 12:31:46 -0700

    add to putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
+#include <stdio.h>
+
 /**
  * main - main block
  * Description: print out "_putchar"
  * Result: 0
  */
 
-int _putchar(char c)
-{
-printf("%s", c);
-}
-
 int main(void)
 {
-_putchar("_putchar");
-return (0);
+  _putchar("_putchar");
+  return (0);
 }
