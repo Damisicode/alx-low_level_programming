2c7d20b8e01da7f7c13f4e00bbb9ba79fa7f3d59
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-15 08:27:48 -0700

    Task2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,14 @@
 
 /**
  * _isdigit - checks for a digit(0 through 9)
- * @c: int c
+ * @c: int type param
  * Return: 1 if c is digit and 0 otherwise
  */
 
 int _isdigit(int c)
 {
 	int result;
+
 	if (c > 47 && c < 58)
 		result = 1;
 	else
