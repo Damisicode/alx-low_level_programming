08b83a7fa23018e67e5249421a02feb01cc704fc
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-19 11:40:00 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen: returns the length of a string
+ * @s: character to be measured
+ * return: length of the string
+ */
+
+int _strlen(char *s)
+{
+  int res = 0;
+  int i;
+  for (i = 0; *(s + i); i++)
+    res += 1;
+	return (res);
+}
