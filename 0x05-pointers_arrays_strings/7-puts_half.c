3e7eede253acbfcf605ff066d3f71511de1d5876
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-20 03:46:11 -0700

    Adding changes

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,10 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	l = i;
+	l = ++i;
 	n = (l - 1) / 2;
 	for (j = 0; l >= j; j++)
-		if ((j > l / 2) && (j > n))
+		if ((j >= l / 2) && (j > n))
 			_putchar(str[j]);
 
 	_putchar('\n');
