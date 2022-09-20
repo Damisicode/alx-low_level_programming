fc03b548b15da196084f7c325a4242938f1909e0
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-20 05:24:17 -0700

    Adding changes

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,21 +12,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, l, j, k, n, m;
+	int i;
 
-	for (j = 0; dest[j] != '\0'; j++)
-		;
-	l = j;
-
-	for (k = 0; src[k] != '\0'; k++)
-		;
-	m = ++k;
-
-	for (i = 0; i < m; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		n = l + i;
-		*(dest + n) = src[i];
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
