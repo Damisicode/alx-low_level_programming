9b096b564d26c8883ff3cf34d40bd83b3d9d2dfa
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-21 12:51:57 -0700

    Adding changes

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,14 @@
 
 char *rot13(char *c)
 {
-	int i;
-	char arr[53] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char arra[53] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j, p;
+	char arr[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arra[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; c[i] != '\0'; i++)
-		if ((c[i] > 64 && c[i] < 91) || (c[i] > 96 && c[i] < 123))
-			c[i] = (c[i] - 64 > 25) ?
-				arra[c[i] - 97] : arr[c[i] - 65];
+		for (j = 0; j < 53; j++)
+			if (c[i] == arr[j])
+				c[i] = arra[j];
 
 	return (c);
 }
