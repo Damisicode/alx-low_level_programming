8e2f923ac34596e74c27adaf09d9d01b415f507a
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-20 17:44:29 -0700

    Adding 101_keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int total;
+	int count;
+	int my_rand;
+
+	srand(time(NULL));
+
+	for (count = 0, total = 2772; total < 122; count++)
+	{
+		my_rand = (rand() % 125) + 1;
+		printf("%c", my_rand);
+		total -= my_rand;
+	}
+	printf("%c", total);
+
+	return (0);
+}
