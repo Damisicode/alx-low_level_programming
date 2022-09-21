d9c9ed7a4f4c16a012c685973ee52ba4b2d0d545
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-21 02:53:09 -0700

    Adding *_strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies string @src to @dest using at most @n bytes
+ * @dest: target string
+ * @src: string to be copy
+ * @n: number of bytes
+ *
+ * Return: target string @dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j, l;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (l = 0, j = 0; src[j] != '\0' && j < n; l++, j++)
+		*(dest + l) = src[j];
+	if (j < n)
+	dest[l] = '\0';
+
+	return (dest);
+}
