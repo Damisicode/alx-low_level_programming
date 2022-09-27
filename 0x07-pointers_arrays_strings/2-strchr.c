6337a5a0751f4373cd850c7bd027869a781bf217
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-26 18:22:17 -0700

    Adding changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		else if (*(s + 1) == c)
-			return (s + 1);
-	}
+	int i;
+	char *p;
 
-	return (s + 1);
+	p = s;
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (p + i);
+	return ('\0');
 }
