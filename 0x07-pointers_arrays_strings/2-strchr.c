4748cef1788ef172c48a8f29c9bcf648b7abe496
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-26 17:48:35 -0700

    Adding changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strchr(char *s, char c)
+ * *_strchr - returns first location of a character
  * @s: charater string
  * @c: character to be located
  * Return: pointer to first location of c
@@ -16,5 +16,6 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 		if (s[i] == c)
 			return (ptr + i);
+
 	return ('\0');
 }
