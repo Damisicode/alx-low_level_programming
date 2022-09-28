3885335cebee7a7068eb9c5c053fc452903eaa06
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-28 14:48:03 -0700

    Adding changes

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,13 @@
 /**
  * _strlen_recursion - gives the length of a string
  * @s: string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
 	if (s[0] == '\0')
-		return 0;
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
