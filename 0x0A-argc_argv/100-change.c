30d14bcdbfd2e6f994adf252210d2a4e7eb840e6
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-09-30 13:43:30 -0700

    Adding changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,6 +3,46 @@
 #include <stdlib.h>
 
 /**
+ * checker - checks for available cent denomination
+ * @num: integer number to count
+ * Return: integer number of denomination needed
+ */
+
+int checker(int num)
+{
+	int total = 0;
+
+	if (num / 25 >= 1)
+	{
+		total += num / 25;
+		num %= 25;
+	}
+
+	if (num / 10 >= 1)
+	{
+		total += num / 10;
+		num %= 10;
+	}
+	if (num / 5 >= 1)
+	{
+		total += num / 5;
+		num %= 5;
+	}
+	if (num / 2 >= 1)
+	{
+		total += num / 2;
+		num %= 2;
+	}
+	if (num / 1 >= 1)
+	{
+		total += num / 1;
+		num %= 1;
+	}
+
+	return (total);
+}
+
+/**
  * main - main block
  * @argc: integer number of argument passed
  * @argv: array of arguments
@@ -13,55 +53,21 @@
 
 int main(int argc, char *argv[])
 {
-	int total, num;
-
+	int num;
 	if (argc == 2)
 	{
-		total = 0;
 		num = atoi(argv[1]);
 		if (num >= 0)
 		{
-			if (num / 25 >= 1)
-			{
-				total += num / 25;
-				num %= 25;
-			}
-
-			if (num / 10 >= 1)
-			{
-				total += num / 10;
-				num %= 10;
-			}
-
-			if (num / 5 >= 1)
-			{
-				total += num / 5;
-				num %= 5;
-			}
-
-			if (num / 2 >= 1)
-			{
-				total += num / 2;
-				num %= 2;
-			}
-
-			if (num / 1 >= 1)
-			{
-				total += num / 1;
-				num %= 1;
-			}
-			printf("%d\n", total);
+			printf("%d\n", checker(num));
 		}
-
 		else
 			printf("0\n");
 	}
-
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }
