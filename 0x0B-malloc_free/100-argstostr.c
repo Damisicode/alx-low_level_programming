2cbc8216723e1013e2dd1d9a9166c04cf8a0efad
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-04 20:57:44 -0700

    Adding strtow

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenates all arguments of the program
+ * @ac: argument count
+ * @av: argument variables
+ * Return: pointer to a new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *a, *retp;
+	int i, j, total;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
+	}
+	total++;
+
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	retp = a;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*a = av[i][j];
+			a++;
+		}
+		*a = '\n';
+		a++;
+	}
+
+	return (retp);
+}
