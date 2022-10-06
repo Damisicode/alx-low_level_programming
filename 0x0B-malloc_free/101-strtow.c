9ade6773b4e7bb63b864ce9a48a670c351520f19
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-06 04:03:32 -0700

    Adding changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,17 +1,15 @@
-
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * str_len - gives the length of a string
+ * str_len - measures the length of a string
  * @str: string to be measured
- * Return: integer length of string
+ * Return: integer number of words
  */
 
-int str_len(char str[])
+int str_len(char *str)
 {
 	int i;
-
 	for (i = 0; str[i]; i++)
 		;
 	return (i);
@@ -31,7 +29,7 @@ int word_length(char *str)
 	words = 0;
 	while (str[i])
 	{
-		if (str[i] == ' ' && *(str + i - 1) != ' ' && i != 0)
+		if (str[i] == ' ' && *(str + i - 1) != ' ')
 			words++;
 		i++;
 	}
@@ -46,47 +44,44 @@ int word_length(char *str)
 
 char **strtow(char *str)
 {
-	int i, j, l, m, n, size;
 	char **ptr;
+	int i, j, temp, size, words;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-
-	ptr = (char **)malloc(sizeof(char *) * (word_length(str) + 1));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	i = 0;
-	l = 0;
-	while (str[i])
-	{
-		if (str[i] != ' ' && str[i] != '\0')
-		{
-			l = i;
-			for (j = 0; str[l] != ' '; j++)
-				l++;
-			size = j;
-
-			ptr[n] = (char *)malloc(sizeof(char) * size + 1);
-
-			if (ptr[n] == NULL)
-				return (NULL);
-
-			m = 0;
-			while (m < size)
+	i = 0; j = 0; temp = 0; size = 0; words = word_length(str);
+	if (words == 0) return (NULL);
+	ptr = (char **)malloc(sizeof(char *) * (words + 1));
+	if (ptr != NULL){
+		while (i <= str_len(str) && words){
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] == ' ') || (str[i] == '\0')) &&
+				 i && (str[i - 1] != ' '))
 			{
-				ptr[n][m] = str[i];
-				i++;
-				m++;
+				ptr[j] =
+					(char *)malloc(sizeof(char) * size + 1);
+				if (ptr[j] != NULL){
+					while (temp < size){
+						ptr[j][temp] =
+							str[(i - size) + temp];
+						temp++;
+					}
+					ptr[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(ptr[j]);
+					free(ptr);
+					return (NULL);
+				}
 			}
-
-			ptr[n][m] = '\0';
-			n++;
+			i++;
 		}
-		i++;
+		ptr[words] = NULL;
+		return (ptr);
 	}
-	ptr[n] = '\0';
-
-	return (ptr);
+	else
+		return (NULL);
 }
