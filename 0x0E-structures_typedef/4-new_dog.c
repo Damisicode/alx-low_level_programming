aaf4aaed36efad7c7648e7dccf36de4bfd318dc9
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-12 23:25:11 -0700

    Adding changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,8 +24,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	N_dog->name = malloc((name_len + 1) * sizeof(char));
 	if (N_dog->name == NULL)
+	{
 		free(N_dog->name);
 		return (NULL);
+	}
 
 	else
 	{
@@ -43,6 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(N_dog->owner);
 		free(N_dog->name);
 		return (NULL);
+	}
 
 	else
 	{
