deeb2ddfcee1210317bb388466a6833c779f55f8
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-14 06:37:51 -0700

    Adding changes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,24 +11,32 @@
 
 int main(int argc, char *argv[])
 {
-	int result, first, third;
-	(void)argc;
+	int first, third;
+	int (*result)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	first = atoi(argv[1]);
 	third = atoi(argv[3]);
-	result = get_op_func(argv[2])(first, third);
+	result = get_op_func(argv[2]);
 
-	if (result != '\0')
-		printf("%d\n", result);
-	else
+	if (result == NULL)
 	{
-	printf("Error\n");
-	exit(99);
+		printf("Error\n");
+		exit(99);
 	}
+
+	printf("%d\n", result(first, third));
+
 	return (0);
 }
