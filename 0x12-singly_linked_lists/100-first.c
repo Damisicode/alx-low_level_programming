e2aa40e9eaed13493da24889201e9133bf9854bb
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-20 21:49:01 -0700

    Adding changes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * before_main - prints string before main is executed
+ * before_main - Print statement within this fxn before running main fxn.
  */
-
-void before_main() __attribute__((constructor));
-
-void before_main()
+void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
