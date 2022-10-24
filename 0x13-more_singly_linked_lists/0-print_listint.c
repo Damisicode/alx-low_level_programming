5d58a67986e40e9c880795a5d4f0bb8a94b1c9f4
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-24 16:08:04 -0700

    Adding changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,7 +16,7 @@ int print_listint(const listint_t *h)
 	count = 0;
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
-		return (NULL);
+		return (0);
 	temp->n = h->n;
 	temp->next = h->next;
 	while (temp != NULL)
