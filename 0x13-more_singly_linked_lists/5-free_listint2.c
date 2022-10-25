e5b9f1da2df6c7cb9806bdcd00ec0d8316f678e0
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-25 14:05:47 -0700

    Adding changes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,7 @@ void free_listint2(listint_t **head)
 	while (head != NULL)
 	{
 		hold = *head;
-		free(head);
-		*head = hold->next;
+		*head = (*head)->next;
 		free(hold);
 	}
-	head = NULL;
-	free(head);
-	free(*head);
 }
