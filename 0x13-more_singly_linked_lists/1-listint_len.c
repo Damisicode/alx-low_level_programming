8d5e0fa06ebf9c49d53f48639e6205af468c31bc
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-25 05:20:20 -0700

    Adding changes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len - determine the number of elements in a linked list
+ * @h: list head
+ * Return: Number of elements
+ */
+
+int listint_len(const listint_t *h)
+{
+	listint_t *temp;
+	int count;
+
+	count = 0;
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	temp->n = h->n;
+	temp->next = h->next;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	free(temp);
+	return (count);
+}
