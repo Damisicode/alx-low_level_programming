ec575b88dfde2dcdf2d6990169e0701a9c59f156
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-25 20:59:14 -0700

    Adding changes to 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,27 +8,19 @@
  * Return: the number of nodes
  */
 
-int print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	listint_t *temp;
-	size_t count;
+	const listint_t *temp;
+	int count;
 
 	count = 0;
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	temp->n = h->n;
-	temp->next = h->next;
+	temp = h;
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
 		count++;
 	}
-	free(temp);
 
 	return (count);
 }
