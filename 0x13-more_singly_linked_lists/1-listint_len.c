536b91455e3d9f2e375e34eb4300589ab9c7839a
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-25 21:07:34 -0700

    Adding changes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,24 +10,15 @@
 
 size_t listint_len(const listint_t *h)
 {
-	listint_t *temp;
+	const listint_t *temp;
 	unsigned int count;
 
 	count = 0;
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	temp->n = h->n;
-	temp->next = h->next;
-
+	temp = h;
 	while (temp != NULL)
 	{
 		count++;
 		temp = temp->next;
 	}
-	free(temp);
 	return (count);
 }
