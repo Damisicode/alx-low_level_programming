f157dee69ac378ca4e60f984d943b2b82e3d95be
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-28 12:32:44 -0700

    Adding 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,2 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: long integer to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
