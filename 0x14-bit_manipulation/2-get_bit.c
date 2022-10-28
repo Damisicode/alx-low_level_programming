35d5a53a22b738de925b9bde7fd8a623db8a72cb
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-28 12:53:53 -0700

    Adding changes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,27 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int temp;
-	int shifts, pos;
+	unsigned long int hold;
 
-	pos = index;
-	if (n == 0)
-	{
+	if (index > 64)
 		return (-1);
-	}
 
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-		;
+	hold = n >> index;
 
-	for (; shifts >= 0; shifts--)
-	{
-		if (shifts == pos)
-		{
-			if ((n >> shifts) & 1)
-				return (1);
-			else
-				return (0);
-		}
-	}
-	return (-1);
+	return (hold & 1);
 }
