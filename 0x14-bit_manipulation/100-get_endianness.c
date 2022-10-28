b5a54668e7e90f899fc8fcdfbe69244f3f8fc795
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-10-28 11:59:39 -0700

    Adding 100-get_endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,19 @@
-#include 
+#include "main.h"
+
+/**
+ * get_endianness - checks for endianess
+ * Return: 0 if big endian and 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *p = (char *) &n;
+
+	if (p)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
