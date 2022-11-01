bac53f500f68370658425189430d1c21e7de0233
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-11-01 12:40:15 -0700

    Adding changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	size = write(fd, text_content, i);
+
+	if (text_content == NULL)
+		size = write(fd, "", i);
+	else
+		size = write(fd, text_content, i);
 
 	if (size < 0)
 		return (-1);
